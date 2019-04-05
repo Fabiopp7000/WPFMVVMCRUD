1701db8640ae92feea73039c60caf16ad7590938
Fabiopp7000 <tribalfabio@hotmail.com>
2019-04-04 23:52:43 -0300

    Implementação Framework MVVM Light

--- WPFMVVMCRUD/MainWindow.xaml.cs
+++ WPFMVVMCRUD/MainWindow.xaml.cs
@@ -23,7 +23,6 @@ namespace WPFMVVMCRUD
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new ViewModel.FuncionariosViewModel();
         }
     }
 }
